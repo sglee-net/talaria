d3429fc0f0b409e0a521e2671dc23ad8815350d6
sglee <sg10.lee@gmail.com>
2018-02-14 09:05:16 +0900

    update websocket task to do : get properties without springboot (client)

---
+++ src/main/java/net/sglee/talaria/websocket/client/Application.java
@@ -0,0 +1,10 @@
+package net.sglee.talaria.websocket.client;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RunClient.start(args);
+	}
+
+}
