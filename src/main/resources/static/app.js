37af226d0636afa8948da4ce0434bcf4bf4c929d
sglee <sg10.lee@gmail.com>
2018-02-26 19:25:35 +0900

    update graph

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -54,3 +54,35 @@ $(function () {
     $( "#disconnect" ).click(function() { disconnect(); });
     $( "#send" ).click(function() { sendMessage(); });
 });
+
+$(document).ready(function () {
+	var data_size = 1000;
+    var data = [];
+//    var x_init = new Date();
+//    var y_init = 0;
+    data.push([new Date(), 0]);
+
+//    for (var i = 10; i >= 0; i--) {
+//      var x = new Date(t.getTime() - i * 1000);
+//      data.push([x, Math.random()]);
+//    }
+
+    var g = new Dygraph(document.getElementById("div_g"), data,
+                        {
+                          drawPoints: true,
+                          showRoller: true,
+                          valueRange: [0.0, 1.2],
+                          labels: ['Time', 'Value']
+                        });
+    // It sucks that these things aren't objects, and we need to store state in window.
+    window.intervalId = setInterval(function() {
+      var x = new Date();  // current time
+      var y = Math.random();
+      data.push([x, y]);
+      if(data.length > data_size) {
+    	  data.shift();
+      }
+      g.updateOptions( { 'file': data } );
+    }, 1000);
+  }
+);
