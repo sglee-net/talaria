888324bc54824520e2745abed110b14ed48197d6
sglee <sg10.lee@gmail.com>
2018-06-12 22:53:33 +0900

    update thrift handler

--- src/main/java/org/chronotics/talaria/thrift/ThriftServer.java
+++ src/main/java/org/chronotics/talaria/thrift/ThriftServer.java
@@ -10,30 +10,64 @@ import org.apache.thrift.transport.TServerSocket;
 import org.apache.thrift.transport.TServerTransport;
 
 import org.chronotics.talaria.thrift.gen.TransferService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.apache.thrift.transport.TSSLTransportFactory.TSSLTransportParameters;
 
 public class ThriftServer {
+	private static final Logger logger = 
+			LoggerFactory.getLogger(ThriftServer.class);
 	
-	public static void startServer(
+	private static TransferService.Processor<TransferService.Iface> processor = null;
+	private static TServer server = null;
+	private static TServerTransport serverTransport = null;
+	private static Runnable runnable = null;
+	
+	public static synchronized void startServer(
 			TransferService.Iface _serviceHandler, 
-			ThriftServerProperties _properties) {        
-		try {			
-			final TransferService.Processor<TransferService.Iface> processor = 
-					new TransferService.Processor<TransferService.Iface>(_serviceHandler);
+			ThriftServerProperties _properties) {
+		if(server != null) {
+			if(!server.isServing()) {
+				server.serve();
+				logger.info("TServer is restarted");
+			}
+			logger.info("TServer is already started");
+			return;
+		}
+		
+		try {
+			if(processor == null) {
+				processor = new TransferService
+						.Processor<TransferService.Iface>(_serviceHandler);
+			}
 			
-			Runnable server = new Runnable() {
-				public void run() {
-					server(processor, _properties);
-				}
-			};      	      
-			new Thread(server).start();
+			if(runnable == null) {
+				runnable = new Runnable() {
+					public void run() {
+						server(processor, _properties);
+					}
+				};
+				
+				new Thread(runnable).start();
+			}
 		} catch (Exception x) {
 			x.printStackTrace();
 		}
 	}
-    
-	public static void server(
+	
+	public static synchronized void stopServer() {
+		if(server != null) {
+			if(server.isServing()) {
+				logger.info("TServer is stopped");
+				server.stop();
+			} else {
+				logger.info("TServer is already stopped");
+			}
+		}
+	}
+   
+	private static void server(
 			TransferService.Processor<TransferService.Iface> processor, 
 			ThriftServerProperties _properties) {
 		try {			
@@ -41,22 +75,31 @@ public class ThriftServer {
 			int port = Integer.parseInt(_properties.getPort()); //9091;//
     		
 			InetAddress listenAddress = InetAddress.getByName(ip);
-			TServerTransport serverTransport = new TServerSocket(
-    				new InetSocketAddress(listenAddress, port));
+			if(serverTransport == null) {
+				serverTransport = new TServerSocket(
+    							new InetSocketAddress(listenAddress, port));
+			}
     		// Simple server
-    		//TServer server = new TSimpleServer(new Args(serverTransport).processor(processor));
+    		// TServer server = new TSimpleServer(new Args(serverTransport).processor(processor));
     		// Use this for a multithreaded server
-    		TServer server = 
-    				new TThreadPoolServer(
-    						new TThreadPoolServer.Args(serverTransport).processor(processor));
-    		System.out.println("Starting the simple server...");
-    		server.serve();
+			if(server == null) {
+				server = new TThreadPoolServer(
+							new TThreadPoolServer
+							.Args(serverTransport)
+							.processor(processor));
+			}
+    		
+    		if(!server.isServing()) {
+    			server.serve();
+    		}
+			
+			logger.info("Starting the TThreadPoolServer server...");
     	} catch (Exception e) {
     		e.printStackTrace();
     	}
 	}
 	
-	public static void secure(
+	private static void secure(
 			TransferService.Processor<TransferService.Iface> processor, 
 			ThriftServerProperties _properties) {
 		int port = Integer.parseInt(_properties.getSecurePort());
@@ -81,14 +124,26 @@ public class ThriftServer {
 			 * Note: You need not explicitly call open(). The underlying server socket is bound on return
 			 * from the factory class. 
 			 */
-			TServerTransport serverTransport = TSSLTransportFactory.getServerSocket(port, 0, null, params);
-			//TServer server = new TSimpleServer(new Args(serverTransport).processor(processor));
+			if(serverTransport == null) {
+				serverTransport = 
+						TSSLTransportFactory
+						.getServerSocket(port, 0, null, params);
+			}
 			
+			//TServer server = new TSimpleServer(new Args(serverTransport).processor(processor));
 			// Use this for a multi threaded server
-			TServer server = new TThreadPoolServer(
-					new TThreadPoolServer.Args(serverTransport).processor(processor));
-			System.out.println("Starting the secure server...");
-			server.serve();
+			if(server == null) {
+				server = new TThreadPoolServer(
+							new TThreadPoolServer
+							.Args(serverTransport)
+							.processor(processor));
+			}
+				
+			if(!server.isServing()) {
+    			server.serve();
+    		}
+				
+			logger.info("Starting the TThreadPoolServer server...");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
---
+++ src/test/java/org/chronotics/talaria/thrift/MessageTransferThroughThriftServer.java
@@ -0,0 +1,129 @@
+package org.chronotics.talaria.thrift;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.chronotics.talaria.common.MessageQueue;
+import org.chronotics.talaria.common.MessageQueueMap;
+import org.chronotics.talaria.impl.ThriftWithMessageQueue;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {ThriftServerProperties.class})
+public class MessageTransferThroughThriftServer {
+	@Autowired
+	private ThriftServerProperties thriftServerProperties;
+
+	private static List<String> keyList = null;
+	private static int keySize = 100;
+	private static String message = "{\n" + 
+			"  \"timestamp\" : 1528804317,\n" + 
+			"  \"sender_id\": \"sender\",\n" + 
+			"  \"receiver_id\": \"receiver\",\n" + 
+			"  \"command\": \"execute training\"\n" + 
+			"}\n" + 
+			"";
+	private static long delay = 5000;
+	
+	public void startServer() {
+		// start thrift server
+		ThriftService thriftServiceHandler = new ThriftWithMessageQueue();
+		ThriftServer.startServer(thriftServiceHandler,thriftServerProperties);		
+	}
+	
+	@BeforeClass
+	public static void setup() {
+		keyList = new ArrayList<String>();
+		for(int i=0; i<keySize; i++) {
+			keyList.add("id"+String.valueOf(i));
+		}
+	}
+	
+	@AfterClass
+	public static void teardown() {
+		ThriftServer.stopServer();
+	}
+	
+	@Test
+	public void getQueueIdWithMessageQueueInsertion() {
+		startServer();
+		MessageQueueMap mqMap = MessageQueueMap.getInstance();
+		for(int i=0; i<keySize; i++) {
+			// register message queue
+			MessageQueue<String> mq = 
+					new MessageQueue<String>(
+							String.class,
+							MessageQueue.default_maxQueueSize,
+							MessageQueue.OVERFLOW_STRATEGY.DELETE_FIRST);
+			boolean rt = mqMap.put(keyList.get(i), mq);
+			assertTrue(rt==true);
+		}
+		for(int i=0; i<keySize; i++) {
+			MessageQueue<String> mq = 
+					(MessageQueue<String>) 
+					mqMap.getMessageQueue(keyList.get(i));
+			assertTrue(mq!=null);
+		}
+	}
+	
+	@Test
+	public void getQueueIdWithoutMessageQueueInsertion() {
+		startServer();
+		MessageQueueMap mqMap = MessageQueueMap.getInstance();
+		for(int i=0; i<keySize; i++) {
+			MessageQueue<String> mq = 
+					(MessageQueue<String>) 
+					mqMap.getMessageQueue(keyList.get(i));
+			assertTrue(mq==null);
+		}
+	}
+	
+	@Test
+	public void insertMessage() {
+		startServer();
+		MessageQueueMap mqMap = MessageQueueMap.getInstance();
+		for(int i=0; i<keySize; i++) {
+			MessageQueue<String> mq = 
+					(MessageQueue<String>) 
+					mqMap.getMessageQueue(keyList.get(i));
+			if(mq==null) {
+				mq = new MessageQueue<String>(
+							String.class,
+							MessageQueue.default_maxQueueSize,
+							MessageQueue.OVERFLOW_STRATEGY.DELETE_FIRST);
+				mqMap.put(keyList.get(i), mq);
+			}
+		}
+		for(int i=0; i<keySize; i++) {
+			MessageQueue<String> mq = 
+					(MessageQueue<String>) 
+					mqMap.getMessageQueue(keyList.get(i));
+			mq.add(String.valueOf(i));
+		}
+		
+		try {
+			Thread.sleep(delay);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		assertTrue(true);
+		
+		for(int i=0; i<keySize; i++) {
+			MessageQueue<String> mq = 
+					(MessageQueue<String>) 
+					mqMap.getMessageQueue(keyList.get(i));
+			assertTrue(mq.size()==0);
+		}
+	}
+}
